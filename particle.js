669194ff755981a60dcba6cf3aa23b1489ee62c1
AnoushkaGharat <83427391+AnoushkaGharat@users.noreply.github.com>
2021-07-12 07:16:32 -0700

    Update particle.js

--- particle.js
+++ particle.js
@@ -1,16 +1,26 @@
 class Particle {
     constructor(x, y, r) {
+        var options={
+            restitution:0.5
+        }
         //console.log("Particle.constructor... this.x:" + x + " this.y:" + y + " this.r:" + r)
         this.radius = r;
-        this.body = Bodies.circle(x, y, this.radius);
+        this.body = Bodies.circle(x, y, this.radius,options);
         this.color = color(random(0,255), random(0,255), random(0,255));
         World.add(world, this.body);
     }
 
     display() {
         var pos = this.body.position;
+
+        var angle=this.body.angle;
+
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
         rectMode(CENTER);
         fill(this.color);
-        circle(pos.x, pos.y, this.radius);
+        ellipse(0, 0, this.radius,this.radius);
+        pop();
     }
 }
